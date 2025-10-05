72b35bf751f9804a5cf68aabafab626981d096c9
aanandpanthangi <aanandpanthangi2801@gmail.com>
2025-10-04 20:43:06 -0500

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,24 +4,32 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-                git 'https://github.com/YOUR-USERNAME/jenkins-sample-app.git'
+                git 'https://github.com/aanandpanthangi/jenkins-simple-app.git'
             }
         }
+
         stage('Build') {
             steps {
                 echo "Building project..."
+                bat 'echo Build step executed successfully!'
             }
         }
+
         stage('Test') {
             steps {
                 echo "Running tests (placeholder, since this is static HTML)"
+                bat 'echo Test step executed successfully!'
             }
         }
+
         stage('Deploy to Staging') {
             steps {
-                sh 'mkdir -p /var/www/staging'
-                sh 'cp -r * /var/www/staging/'
-                echo "App deployed to Staging!"
+                echo "Deploying to staging directory..."
+                bat '''
+                    if not exist C:\\staging mkdir C:\\staging
+                    xcopy * C:\\staging /E /I /Y
+                '''
+                echo "App deployed to C:\\staging successfully!"
             }
         }
     }
